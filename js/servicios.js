71ec93059be90ebbff0cb30aca02bd8fabc7207d
NadiaAlvarezAll <kyusshi00sebass@gmail.com>
2026-08-24 16:34:27 -0400

    servicios.js

--- js/servicios.js
+++ js/servicios.js
@@ -55,13 +55,19 @@
                 ]
             };
 
+            // CARRITO
+            let carrito = [];
+
+            // GENERA BLOQUE
             const contenedor_Productos = document.getElementById("contenedorProductos");
 
             respuestaProductos.data.forEach(producto => {
 
+                // COLUMNA 
                 const columna = document.createElement("div");
                 columna.className = "col-12 col-md col-lg-4";
 
+                // BLOQUE 
                 const bloque = document.createElement("div");
                 bloque.className = "card h-100 shadow";
 
@@ -107,3 +113,126 @@
                 contenedor_Productos.appendChild(columna);
 
             });
+
+            // AGREGAR PRODUCTOS AL CARRITO 
+
+            // function para calculo
+            function agregarAlCarrito(id) 
+            {
+
+                // Busca el producto por el id 
+                // === para realizar una comparacion de igualdad estricta
+                // => ARROW FUNCTIONS funciones con flechas 
+                const producto = respuestaProductos.data.find(producto => producto.id === id);
+
+                // BUSCAR SI YA SE TIENE EL PRODUCTO EN EL CARRITO 
+                const productoExiste = carrito.find(producto => producto.id === id);
+
+                if (productoExiste) 
+                {
+
+                    // SI EL PRPDIUCTO YA EXISTE AUMENTE EN LA CANTIDAD
+                    productoExiste.cantidad++;
+
+                }
+                else 
+                {
+
+                    // SO NO EXISTE ENB EL CARRITO SE AGREGA
+                    // .push es para agergar uno o mas al elemento final
+                    carrito.push
+                    ({
+
+                        // ... spread operator ,,, 
+                        // toma las propiedades del producto y las copia dentro del nuevo obj
+                        ...producto,
+
+                        cantidad: 1
+
+                    });
+
+                }
+
+                // ACTUALIZAR CARRITO
+                actualizarCarrito();
+
+            }
+
+            function actualizarCarrito()
+            {
+
+                const listaCarrito = document.getElementById("listaCarrito");
+                const contadorCarrito = document.getElementById("contadorCarrito");
+                const totalCarrito = document.getElementById("totalCarrito");
+
+                // VACIAR EL CARRITO 
+                // CAMBIAR CONTENIDO DEL HTML
+                listaCarrito.innerHTML = "";
+
+                // CARRITO VACIO
+                // .length PARA SABER LA CANTIDAD DE ELEMENTOS O CARACTERES O PARAAMETROS QUE SE TIENE
+                if (carrito.length === 0) 
+                {
+
+                    // <p> texto en pantalla
+                    listaCarrito.innerHTML = `
+                    
+                    <p class = "text-muted">
+                        El carrito esta vacio
+                    </p>
+                    `;
+
+                    contadorCarrito.textContent = 0;
+                    totalCarrito.textContent = 0;
+
+                }
+
+                let total = 0;
+                let cantidadTotal = 0;
+
+                // LEER TODOS LOS PROCUTOS DEL CARRITO "RECORRER"
+                carrito.forEach(producto => 
+                {
+
+                    const subTotal = producto.precio * producto.cantidad;
+                    total += subTotal;
+                    cantidadTotal += producto.cantidad;
+
+                    // <h6> titulo enano 
+                    // MOSTRASTRAR LOS PRODUCTOR 
+                    listaCarrito.innerHTML += `
+                    
+                    <div class="card mb-2">
+                        <div class"card-body">
+
+                            <h6>
+
+                                ${producto.nombre}
+
+                            </h6>
+
+                            <p class="mb-1">
+                                precio:
+                                $${producto.precio.toLocaleString("es-CL")}
+                            </p>
+
+                            <p class="mb-1">
+                                cantidad:
+                                ${producto.cantidad}
+                            </p>
+
+                            <strong>
+                                subtotal:
+                                $${subTotal.toLocaleString("es-CL")}
+                            </strong>
+                        </div>
+                    </div>
+
+                    `;
+
+                });
+
+                contadorCarrito.textContent = cantidadTotal;
+                totalCarrito.textContent = total.toLocaleString("es-Cl");
+
+            }
