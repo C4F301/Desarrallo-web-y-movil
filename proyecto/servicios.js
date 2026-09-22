3dc61f555d026e6c863a5caf97cc0fe2a36a8385
NadiaAlvarezAll <kyusshi00sebass@gmail.com>
2026-09-21 21:09:21 -0300

    carrito

---
+++ proyecto/servicios.js
@@ -0,0 +1,92 @@
+
+const respuestaPizzas = 
+{
+
+    "status": 200,
+    "message": "Productos obtenidos correctamente",
+    "dato": 
+    [
+        {
+
+            _id: "6a9f1d1d290a1c36dee29c7e",
+            nombre: "margarita",
+            ingredientes: ["Salsa de tomate", "Mozzarella", "albahaca"],
+            precio: 8000,
+            imagen: "FALTA"
+
+        },
+
+        {
+
+            _id: "6a9f1d1d290a1c36dee29c7f",
+            nombre: "Pepperoni",
+            ingredientes: ["Salsa de tomate", "Mozzarella", "Pepperoni"],
+            precio: 9500,
+            imagen: "FALTA"
+
+        },
+
+        {
+
+            _id: "6a9f1d1d290a1c36dee29c80",
+            nombre: "Hawaiana",
+            ingredientes: ["Salsa de tomate", "Mozzarella", "Jamón", "Piña"],
+            precio: 9000,
+            imagen: "FALTA"
+
+        },
+
+        {
+
+            _id: "6a9f1d1d290a1c36dee29c81",
+            nombre: "Cuatro Quesos",
+            ingredientes: ["Mozzarella", "Parmesano", "Gorgonzola", "Provolone"],
+            precio: 10000,
+            imagen: "FALTA"
+
+        },
+
+        {
+
+            _id: "6a9f1d1d290a1c36dee29c82",
+            nombre: "Vegetariana",
+            ingredientes: ["Salsa de tomate", "Mozzarella", "Pimientos", "Cebolla", "Champiñones"],
+            precio: 9200,
+            imagen: "FALTA"
+
+        },
+
+        {
+
+            _id: "6a9f1d1d290a1c36dee29c83",
+            nombre: "Napolitana",
+            ingredientes: ["Salsa de tomate", "Mozzarella", "Tomate fresco", "Orégano"],
+            precio: 8800,
+            imagen: "FALTA"
+
+        },
+
+        {
+
+            _id: "6a9f1d1d290a1c36dee29c84",
+            nombre: "Carbonara",
+            ingredientes: ["Crema", "Mozzarella", "Tocino", "Parmesano"],
+            precio: 9800,
+            imagen: "FALTA"
+
+        },
+
+        {
+
+            _id: "6a9f1d1d290a1c36dee29c85",
+            nombre: "Mexicana",
+            ingredientes: ["Salsa de tomate", "Mozzarella", "Carne molida", "Jalapeño", "Pimenton"],
+            precio: 10200,
+            imagen: "FALTA"
+
+        }
+
+    ]
+
+};
+
